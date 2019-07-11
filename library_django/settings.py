e164c0c8991d34dcb1dee77fbdaa7f3625b3e09f
neunum <you@example.com>
2019-07-11 16:16:39 +0200

    DEBUG=False

--- library_django/settings.py
+++ library_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = local.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['206.189.40.53']
 
