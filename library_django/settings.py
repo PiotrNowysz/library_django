190e3d2e4c4de5064dcca199475b5c7222677188
neunum <you@example.com>
2019-07-12 10:44:17 +0200

    default language - polish

--- library_django/settings.py
+++ library_django/settings.py
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'pl'
 LANGUAGES = (
     ('pl', 'PL'),
     ('en', ' ENG'),
